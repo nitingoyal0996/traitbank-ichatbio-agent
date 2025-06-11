b3db7a3e73fc6bd4aae1378f8aa8ff758eb22c64
Nitin Goyal <nitin.goyal@ufl.edu>
2025-06-11 15:28:53 +0000

    Clean up chore

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,3 @@
 from .taxons import TaxonDataRequest, TaxonDataResponse
 from .traits import TraitDataRequest, TraitDataResponse
-from .agent import TraitBankRequest
+from .request import TraitBankRequest
